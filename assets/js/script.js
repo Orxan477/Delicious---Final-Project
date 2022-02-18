ac302a2cf93c8f24335465b0624d4b1315f9bb10
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-02-18 17:55:45 +0400

    Slider, text effect and navbar effect were done

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,25 @@ $(document).ready(function(){
       loop:true,
       autoplay:true,
       animateOut: 'fadeOut',
-      autoplayTimeout:5000
+      autoplayTimeout:5000,
+      autoplayHoverPause:true
   });
   });
+
+  
+
+window.onscroll = function() {myFunction()};
+
+// var navbar1 = document.getElementById("nav1");
+var navbar2 = document.getElementById("nav2");
+var sticky = navbar2.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar2.classList.add("sticky");
+    navbar2.classList.add("background-scroll-navbar");
+  } else {
+    navbar2.classList.remove("sticky");
+    navbar2.classList.remove("background-scroll-navbar")
+  }
+}
