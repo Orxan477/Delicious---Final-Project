6505a9111d5c0f8706ed7a3378d8823215a6eab7
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-02-20 03:27:57 +0400

    Special Section was done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
-var owl = $('.owl-carousel');
+var owl = $(".owl-carousel");
 
-$(document).ready(function(){
+$(document).ready(function () {
   owl.owlCarousel({
-      items:1,
-      loop:true,
-      autoplay:true,
-      animateOut: 'fadeOut',
-      autoplayTimeout:7000,
-      autoplayHoverPause:false
+    items: 1,
+    loop: true,
+    autoplay: true,
+    animateOut: "fadeOut",
+    autoplayTimeout: 7000,
+    autoplayHoverPause: false,
   });
-  });
+});
 
-  
-
-window.onscroll = function() {myFunction(),myFunctionNavbar(),myFunctionHomeNavbar()};
+window.onscroll = function () {
+  myFunction(), myFunctionNavbar(), myFunctionHomeNavbar();
+};
 
 var navbar2 = document.getElementById("nav2");
 var sticky = navbar2.offsetTop;
@@ -24,13 +24,13 @@ function myFunction() {
     navbar2.classList.add("background-scroll-navbar");
   } else {
     navbar2.classList.remove("sticky");
-    navbar2.classList.remove("background-scroll-navbar")
-    navbar2.classList.add("background-scroll-0-navbar")
+    navbar2.classList.remove("background-scroll-navbar");
+    navbar2.classList.add("background-scroll-0-navbar");
   }
 }
 
 var about = document.getElementById("about");
-var aboutNav=document.getElementById("about-nav");
+var aboutNav = document.getElementById("about-nav");
 var aboutSticky = about.offsetHeight;
 
 function myFunctionNavbar() {
@@ -42,10 +42,8 @@ function myFunctionNavbar() {
 }
 
 var home = document.getElementById("homeIntro");
-var homeNav=document.getElementById("home-nav");
+var homeNav = document.getElementById("home-nav");
 var homeSticky = home.offsetHeight;
-
-console.log(window.pageYOffset)
 
 function myFunctionHomeNavbar() {
   if (window.pageYOffset >= homeSticky && window.pageYOffset < aboutSticky) {
@@ -53,4 +51,43 @@ function myFunctionHomeNavbar() {
   } else {
     homeNav.classList.remove("active-color");
   }
+}
+
+
+
+
+var specialNames = document.querySelectorAll(".special-name");
+
+[...specialNames].forEach((name) => {
+  name.addEventListener("click", function (ev) {
+
+    ev.preventDefault();
+    ChangeActiveSpecial(name);
+    GetClickData(name);
+    CurrentActiveProperty();
+
+  });
+});
+
+function ChangeActiveSpecial(name) {
+  let currentActiveSpecial = document.querySelector(".active-special");
+  currentActiveSpecial.classList.remove("active-special");
+  name.classList.add("active-special");
+}
+
+async function GetClickData(name) {
+  let clickNameDataId = name.getAttribute("data-id");
+  let clickNameProp = document.getElementById(clickNameDataId);
+  await ChangeActiveProperty(clickNameProp)
+}
+
+function CurrentActiveProperty() {
+  let currentActiveSpecialProp = document.querySelector(".active-special-property");
+  currentActiveSpecialProp.classList.remove("active-special-property");
+  currentActiveSpecialProp.classList.add("d-none");
+}
+
+async function ChangeActiveProperty(clickNameProp){
+  await clickNameProp.classList.remove("d-none");
+  await clickNameProp.classList.add("active-special-property");
 }
