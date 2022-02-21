be67fd0e56b25fb51f51421c69e55d04f971f91a
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-02-22 02:08:00 +0400

    Create reservation page js file

---
+++ assets/js/reservation.js
@@ -0,0 +1,10 @@
+var navbarOtherPage2 = document.getElementById("nav2OtherPage");
+var stickyOther =navbarOtherPage2.offsetTop;
+
+function myFunctionOTherPage() {
+  if (window.pageYOffset > stickyOther ) {
+    navbarOtherPage2.classList.add("stickyOtherPageNavbar");
+  } else {
+    navbarOtherPage2.classList.remove("stickyOtherPageNavbar");
+  }
+}
