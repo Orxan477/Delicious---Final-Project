78209e91ce5b11c4ebe72ef0503cd1b7a301c1ad
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-02-26 01:43:20 +0400

    Front-To-Back and Fixed Front Files

---
+++ Restaurant.UI/wwwroot/assets/js/menu.js
@@ -0,0 +1,25 @@
+var minus = document.querySelector(".minus-click");
+var plus = document.querySelector(".plus-click");
+var count = minus.parentElement.nextElementSibling;
+
+var totalPrice = document.querySelector(".total-price");
+
+var pizza = document.getElementById("pizza");
+var value=pizza.options[pizza.selectedIndex].value;
+
+pizza.addEventListener("click", function () {
+   value = pizza.options[pizza.selectedIndex].value;
+   totalPrice.innerHTML = value * count.innerText;
+});
+
+minus.addEventListener("click", function () {
+  if (count.innerText > 0) {
+    count.innerHTML -= 1;
+    totalPrice.innerHTML = value * count.innerText;
+  }
+});
+
+plus.addEventListener("click", function () {
+  count.innerHTML++;
+  totalPrice.innerHTML = value * count.innerText;
+});
---
+++ Restaurant.UI/wwwroot/assets/js/script.js
@@ -0,0 +1,37 @@
+
+//back-to-home-link
+var upper = document.getElementById("upper");
+function myUpperFunction() {
+  
+  if (window.pageYOffset >= 80) {
+    upper.classList.remove("d-none");
+    upper.classList.add("d-flex");
+  } else {
+    upper.classList.add("d-none");
+    upper.classList.remove("d-flex");
+  }
+}
+
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".nav2Right  ul li .nav-link");
+
+function activeLink() {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    
+    if (pageYOffset >= sectionTop-60) {
+      current = section.getAttribute("id");
+     }
+  });
+  let currentLinkActive=document.querySelector("."+current);
+
+  navLi.forEach((link) => {
+
+    link.classList.remove("active-color");
+    currentLinkActive.classList.add("active-color");
+    
+  });
+};
