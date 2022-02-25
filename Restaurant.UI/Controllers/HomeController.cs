e7fc4873fe9384b06ad227a3d33f1466d05c28c9
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-02-25 17:11:30 +0400

    Layer done

---
+++ Restaurant.UI/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Restaurant.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
