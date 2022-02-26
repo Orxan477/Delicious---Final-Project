7169301e00008aa5359b1ab7dac35049005dc83f
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-02-26 18:32:57 +0400

    Navbar Fixed

--- Restaurant.UI/wwwroot/assets/js/otherPage.js
+++ Restaurant.UI/wwwroot/assets/js/otherPage.js
@@ -1,5 +1,7 @@
 var navbar2 = document.getElementById("nav2");
 var sticky = navbar2.offsetTop;
+var header = document.querySelector(".myNavbar");
+header.classList.add("background-scroll-navbar");
 
 function myFunction() {
   if (window.pageYOffset > sticky ) {
