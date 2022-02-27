f250fcfacb5eaa82ae6d08f7b8366fa64ca815ab
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-02-28 00:53:25 +0400

    Fixed home.js file

--- Restaurant.UI/wwwroot/assets/js/home.js
+++ Restaurant.UI/wwwroot/assets/js/home.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     loop: true,
     autoplay: true,
     animateOut: "fadeOut",
-    autoplayTimeout: 6000,
+    autoplayTimeout: 10000,
     autoplayHoverPause: false
   });
 });
