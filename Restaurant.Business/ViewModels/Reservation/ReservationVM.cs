47ae861444dc0034a5236993ea37592d53b5e350
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-03-02 01:57:37 +0400

    Reservation Form 50% ready

---
+++ Restaurant.Business/ViewModels/Reservation/ReservationVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Restaurant.Business.ViewModels.Reservation
+{
+    public class ReservationVM
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        //public string Time { get; set; }
+        public int PeopleCount { get; set; }
+        public string Message { get; set; }
+    }
+}
