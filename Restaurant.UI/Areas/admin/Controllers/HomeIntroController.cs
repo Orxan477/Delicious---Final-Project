b75cdec5b563a41d223b38ecadd44b2cfb5b75e8
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-03-04 13:11:43 +0400

    HomeIntro Section Admin 20% ready

---
+++ Restaurant.UI/Areas/admin/Controllers/HomeIntroController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Data.DAL;
+
+namespace Restaurant.UI.Areas.admin.Controllers
+{
+    public class HomeIntroController : Controller
+    {
+        private AppDbContext _context;
+
+        public HomeIntroController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.HomeIntros.ToListAsync());
+        }
+    }
+}
