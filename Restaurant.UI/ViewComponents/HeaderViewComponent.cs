43ef5a7e81e5d13b5b3b67705d22370822f7a8b9
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-03-08 01:52:08 +0400

    Navbar basket count done

--- Restaurant.UI/ViewComponents/HeaderViewComponent.cs
+++ Restaurant.UI/ViewComponents/HeaderViewComponent.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Restaurant.Business.ViewModels.Menu;
 using Restaurant.Data.DAL;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Restaurant.UI.ViewComponents
@@ -14,8 +17,19 @@ namespace Restaurant.UI.ViewComponents
         }
         public IViewComponentResult Invoke()
         {
-            ViewBag.basketCount = _context.Products.Count();
+            
+            ViewBag.basketCount = BasketCount();
             return View();
+        }
+        public int BasketCount()
+        {
+
+            if (Request.Cookies["basket"] != null)
+            {
+                List< BasketVM> basket = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+                return basket.Count();
+            }
+            return 0;
         }
     }
 }
