34e9dc2bcf42ee3f973f392e126820658798beef
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-03-08 02:38:23 +0400

    Team Controller Created

---
+++ Restaurant.UI/Areas/admin/Controllers/TeamController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Data.DAL;
+using System.Linq;
+
+namespace Restaurant.UI.Areas.admin.Controllers
+{
+    public class TeamController : Controller
+    {
+        private AppDbContext _context;
+
+        public TeamController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Teams
+                                .Include(x=>x.Position)
+                                .ToList());
+        }
+    }
+}
