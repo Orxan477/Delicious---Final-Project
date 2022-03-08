8bb029160759802eee16c12c30755688d73bfe17
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-03-09 01:06:19 +0400

    Reservation Index page done

---
+++ Restaurant.UI/Areas/admin/Controllers/ReservationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant.Data.DAL;
+using System.Linq;
+
+namespace Restaurant.UI.Areas.admin.Controllers
+{
+    [Area("Admin")]
+    public class ReservationController : Controller
+    {
+        private AppDbContext _context;
+
+        public ReservationController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Reservations.Where(x=>!x.IsDeleted).ToList());
+        }
+    }
+}
