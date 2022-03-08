e7f23fe88728bd0d63f7e08b339d9910a79340a1
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-03-08 23:39:42 +0400

    Reservation page Validator done

--- Restaurant.Business/Validators/Reservation/ReservationVMValidation.cs
+++ Restaurant.Business/Validators/Reservation/ReservationVMValidation.cs
@@ -7,11 +7,11 @@ namespace Restaurant.Business.Validators.Reservation
     {
         public ReservationVMValidation()
         {
-            //RuleFor(x => x.FullName).NotEmpty().NotNull().MaximumLength(50);
-            //RuleFor(x => x.Email).NotEmpty().NotNull().MaximumLength(255).EmailAddress();
-            //RuleFor(x => x.Number);
-            //RuleFor(x => x.Date).NotEmpty().WithMessage("The Date field is required.").NotNull();
-            //RuleFor(x => x.PeopleCount).NotEmpty().WithMessage("The People Count field is required.").NotNull();
+            RuleFor(x => x.FullName).NotEmpty().NotNull().MaximumLength(50);
+            RuleFor(x => x.Email).NotEmpty().NotNull().MaximumLength(255).EmailAddress();
+            RuleFor(x => x.Number).NotEmpty().NotNull();
+            RuleFor(x => x.Date).NotEmpty().NotNull();
+            RuleFor(x => x.PeopleCount).NotEmpty().NotNull();
         }
     }
 }
--- Restaurant.Business/ViewModels/Reservation/ReservationVM.cs
+++ Restaurant.Business/ViewModels/Reservation/ReservationVM.cs
@@ -5,16 +5,10 @@ namespace Restaurant.Business.ViewModels.Reservation
 {
     public class ReservationVM
     {
-        [Required]
         public string FullName { get; set; }
-        [Required, DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        [Required]
-        //[MaxLength(12)]
         public string Number { get; set; }
-        [Required]
         public DateTime Date { get; set; }
-        [Required]
         public int PeopleCount { get; set; }
         public string Message { get; set; }
     }
