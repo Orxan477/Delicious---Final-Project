82a2b63ef292a5de63b0a87237a7f4ac8e6493dc
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-03-08 21:05:35 +0400

    Product Index Page Done || Admin

---
+++ Restaurant.UI/Areas/admin/Controllers/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Data.DAL;
+using System.Linq;
+
+namespace Restaurant.UI.Areas.admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuController : Controller
+    {
+        private AppDbContext _context;
+
+        public MenuController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Products.Include(x => x.MenuImage).Include(x => x.Category).ToList());
+        }
+    }
+}
