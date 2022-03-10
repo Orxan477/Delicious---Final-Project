321e8fdd68db7550aada6def03c5d12ed16dee59
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-03-10 12:45:54 +0400

    Added new migration

---
+++ Restaurant.Data/Migrations/20220310084452_ChooseRestaurantDeleteColumn.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Restaurant.Data.Migrations
+{
+    public partial class ChooseRestaurantDeleteColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "ChooseRestaurants");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Number",
+                table: "ChooseRestaurants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "0");
+        }
+    }
+}
