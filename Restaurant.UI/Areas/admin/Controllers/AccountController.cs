2652e888348595474b18ce8aaebb6beb5e9e4cd6
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-03-15 09:14:18 +0400

    Create AccountController || Admin

---
+++ Restaurant.UI/Areas/admin/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant.Data.DAL;
+
+namespace Restaurant.UI.Areas.admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : Controller
+    {
+        private AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            //return View(_context.User);
+            return View();
+        }
+    }
+}
