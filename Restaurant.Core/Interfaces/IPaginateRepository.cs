db1b8ad9d9d9228aff0aa00af3860038449862c7
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-03-20 00:25:57 +0400

    Fixed IPaginate Repository

--- Restaurant.Core/Interfaces/IPaginateRepository.cs
+++ Restaurant.Core/Interfaces/IPaginateRepository.cs
@@ -7,10 +7,7 @@ namespace Restaurant.Core.Interfaces
 {
     public interface IPaginateRepository<T>
     {
-        //string GetSetting(string entity);  
         Task<List<T>> GetPaginate(int count, int page, Expression<Func<T, bool>> exp = null, params string[] includes );
         int GetPageCount(int take, Expression<Func<T, bool>> expression = null);
-        //List<T> GetProductList(List<T> products);
-        //Task<List<TEntity>> GetAllProduct(Expression<Func<TEntity,bool>> exp=null,params string[] includes);
     }
 }
