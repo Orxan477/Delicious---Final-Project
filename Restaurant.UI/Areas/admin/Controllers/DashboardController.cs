67c1b9c5f1f8d7c2e8e91c86f45f18d534036a07
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2022-03-20 22:27:32 +0400

    Dashboard done

--- Restaurant.UI/Areas/admin/Controllers/DashboardController.cs
+++ Restaurant.UI/Areas/admin/Controllers/DashboardController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant.Business.Interfaces;
+using Restaurant.Business.Interfaces.Setting;
+using Restaurant.Data.DAL;
+using System.Linq;
 
 namespace Restaurant.UI.Areas.admin.Controllers
 {
+        [Area("Admin")]
     public class DashboardController : Controller
     {
-        [Area("Admin")]
+        private AppDbContext _context;
+
+        public DashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            ViewBag.ReservationCount = _context.Reservations.ToList().Count();
+            ViewBag.FullOrderCount=_context.FullOrders.ToList().Count(); 
+            ViewBag.UserCount= _context.Users.ToList().Count();
+            ViewBag.TeamCount= _context.Teams.ToList().Count();
+            ViewBag.ContactUsCount=_context.ContactUs.ToList().Count();
+            ViewBag.SubscribeCount = _context.Subscribes.ToList().Count;
             return View();
         }
     }
